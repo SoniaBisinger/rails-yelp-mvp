dac1de7455fc5455903b30413196a344bce550a7
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-05-24 15:39:49 +0200

    2nd exercices, before refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # A visitor can see the list of all restaurants.
+  get "restaurants", to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end 
+  # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "french"}
+kebabbus =  {name: "Kebabbus", address: "rue de Renens, Renens", category: "belgian"}
+sushoio =  {name: "Sushoio", address: "rue de l'Aeroport 7", category: "japanese"}
+dong_feng =  {name: "Dong Feng", address: "56A Shoreditch High St, London E1 6PQ", category: "french"}
+
+[dishoom, pizza_east, kebabbus, sushoio, dong_feng].each do |attributes|
+    restaurant = Restaurant.create!(attributes)
+    puts "Created #{restaurant.name}"
+  end
+  puts "Finished!"
